a86c4f1c4ad3b9848ed511856b2968ce084ef751
anshab2812 <anshab2812@gmail.com>
2021-10-06 10:50:20 +0300

    V1.1

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QLabel>
  
 int main(int argc, char *argv[ ]) 
 {
 QApplication app(argc, argv);
  
-QPushButton button("Hello, World!");
-button.resize(200, 60);
-button.show( );
+QLabel label("<h2><i>Hello,<i>" "<font size="6">World</font><h2>");
+label.show( );
  
 return app.exec( );
 }
