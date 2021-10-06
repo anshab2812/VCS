a79517665392ef47d1872002dce81cd7802965bc
anshab2812 <anshab2812@gmail.com>
2021-10-06 19:27:38 +0300

    for new branch

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[ ])
 {
 QApplication app(argc, argv);
  
-QLabel label("<h2><i>Hello,<i>" "<font size="6">World</font><h2>");
+QLabel label("<h2><i>Hello,<i>" "<font size="6"> Shabal Anna</font><h2>");
 label.show( );
  
 return app.exec( );
